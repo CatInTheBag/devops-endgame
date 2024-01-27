efcbb76c053727b3f80ce1c535e0cd0bed0ba177
CatInTheBag <vassil.bachvarov@gmail.com>
2024-01-27 11:19:03 +0200

    fixing issue with new version of github runner

--- terraform/modules/webserver/main.tf
+++ terraform/modules/webserver/main.tf
@@ -79,9 +79,9 @@ resource "aws_instance" "myapp-ec2" {
       "sudo apt install -y ansible",
       "cd ~",
       "mkdir actions-runner && cd actions-runner",
-      "curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz",
-      "echo '29fc8cf2dab4c195bb147384e7e2c94cfd4d4022c793b346a6175435265aa278  actions-runner-linux-x64-2.311.0.tar.gz' | shasum -a 256 -c",
-      "tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz",
+      "curl -o actions-runner-linux-x64-2.312.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.312.0/actions-runner-linux-x64-2.312.0.tar.gz",
+      "echo '85c1bbd104d539f666a89edef70a18db2596df374a1b51670f2af1578ecbe031  actions-runner-linux-x64-2.312.0.tar.gz' | shasum -a 256 -c",
+      "tar xzf ./actions-runner-linux-x64-2.312.0.tar.gz",
       "./config.sh --url https://github.com/CatInTheBag/devops-endgame --token ${var.runner_token} --labels self-hosted,ubuntu,ec2 --name aws-ec2 --unattended --replace",
 
       "sudo ./svc.sh install",
